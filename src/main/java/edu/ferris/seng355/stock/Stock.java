034a3ef6299c1e1440e2dbe7098483444147782c
Trevor Busk <buskt@ferris.edu>
2025-02-02 14:58:14 -0500

    Moving Stock to its own package
    Separating concerns

--- src/main/java/edu/ferris/seng355/stock/Stock.java
+++ src/main/java/edu/ferris/seng355/stock/Stock.java
@@ -1,6 +1,7 @@
-package edu.ferris.seng355;
+package edu.ferris.seng355.stock;
 
 public record Stock(
+        String name,
         double price,
         double priceIncreaseProbability,
         double possibleNewPrice
