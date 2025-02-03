9a5fa9472150637c712041cb5c37cb51dfa6dfc5
Trevor Busk <buskt@ferris.edu>
2025-02-03 13:03:39 -0500

    adjusting optimal knapsack algorithm
    Adjusting the optimal knapsack algorithm to return a knapsack with the highest earning stock.  Also adjusting inner logic to hopefully better represent problem.

--- src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
+++ src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
@@ -3,6 +3,8 @@ package edu.ferris.seng355.algorithms;
 import edu.ferris.seng355.items.Item;
 import edu.ferris.seng355.knapsack.Knapsack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -18,31 +20,60 @@ public class OptimalKnapsackAlgorithm implements KnapsackAlgorithm {
 
         int maximumWeight = (int) (maxWeight * 100);
 
-        Knapsack knapsack = new Knapsack(maxWeight);
+        HashMap<Integer, List<Item>> potentialKnapsacks = new HashMap<>();
 
         int[][] table = new int[numberOfItems + 1][numberOfItems + 1];
 
         for (int item = 1; item <= numberOfItems; item++){
-            for (int capacity = 1; capacity < maximumWeight; capacity++) {
+            List<Item> knapsack = new ArrayList<>();
+            for (int capacity = 1; capacity < maximumWeight;) {
                 int maxValueWithoutItem = table[item - 1][capacity];
                 int maxValueWithItem = 0;
                 int currentItemWeight = weights[item - 1];
 
-                if(capacity >= currentItemWeight) {
+                if(maximumWeight > currentItemWeight + capacity) {
                     maxValueWithItem = values[item - 1];
 
                     int remainingCapacity = capacity - currentItemWeight;
                     maxValueWithItem += table[item - 1][remainingCapacity];
+
+
+                    if(maxValueWithItem > maxValueWithoutItem) {
+                        capacity += currentItemWeight;
+                        knapsack.add(availableItems.get(item - 1));
+                    }
                 }
 
-                if(maxValueWithItem > maxValueWithoutItem) {
-                    knapsack.getItems().add(availableItems.get(item - 1));
-                }
                 table[item][capacity] = Math.max(maxValueWithoutItem, maxValueWithItem);
+            }
+            potentialKnapsacks.put(item - 1, knapsack);
+        }
+
+        return getHighestEarningStock(potentialKnapsacks, maxWeight);
+
+    }
+
+    public Knapsack getHighestEarningStock(HashMap<Integer, List<Item>> potentialOptions, double maxWeight) {
+        Knapsack knapsack = new Knapsack(maxWeight);
+        int numberOfOptions = potentialOptions.size();
+        int keyOfMax = -1;
+        double maxTotal = -1;
+
+        for(int index = 0; index < numberOfOptions; index++) {
+            double currentTotal = getTotalItemValue(potentialOptions.get(index));
+
+            if(maxTotal < currentTotal) {
+                keyOfMax = index;
+                maxTotal = currentTotal;
             }
         }
 
-        return knapsack;
+        knapsack.getItems().addAll(potentialOptions.get(keyOfMax));
 
+        return knapsack;
+    }
+
+    public double getTotalItemValue(List<Item> items) {
+        return items.stream().mapToDouble(Item::getValue).sum();
     }
 }
