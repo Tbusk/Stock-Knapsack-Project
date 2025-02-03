7375fce8736110bdef99c1e4b0d7b0108c8c9421
Trevor Busk <buskt@ferris.edu>
2025-02-03 12:19:53 -0500

    adding a test that will generate random stock data csvs and uploading them

---
+++ src/test/java/edu/ferris/seng355/CreateCSVTest.java
@@ -0,0 +1,25 @@
+package edu.ferris.seng355;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class CreateCSVTest {
+
+    @Test
+    void testCreateStocksCSV() throws IOException {
+        RandomStockCSVBuilder csvBuilder = new RandomStockCSVBuilder();
+        csvBuilder.buildCsv(10);
+        csvBuilder.buildCsv(25);
+        csvBuilder.buildCsv(50);
+        csvBuilder.buildCsv(100);
+        csvBuilder.buildCsv(250);
+        csvBuilder.buildCsv(500);
+        csvBuilder.buildCsv(750);
+        csvBuilder.buildCsv(1_000);
+        csvBuilder.buildCsv(5_000);
+        csvBuilder.buildCsv(10_000);
+        csvBuilder.buildCsv(15_000);
+        csvBuilder.buildCsv(25_000);
+    }
+}
