a8e1b0e68a415aceb0e0cb7af329d13e777624d7
Trevor Busk <buskt@ferris.edu>
2025-02-03 17:56:52 -0500

    removing reliance on arrays and ints from optimal knapsack algorithm

--- src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
+++ src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
@@ -11,47 +11,41 @@ public class OptimalKnapsackAlgorithm implements KnapsackAlgorithm {
 
     @Override
     public Knapsack collect(List<? extends Item> availableItems, double maxWeight) {
-        int[] weights = availableItems.stream().mapToInt(item -> (int) (item.getWeight() * 100)).toArray();
-        int[] values = availableItems.stream().mapToInt(item -> (int) (item.getValue() * 100)).toArray();
-
-        int maximumWeight = (int) (maxWeight * 100);
-
-        if(maximumWeight <= 0 || weights.length == 0 || values.length == 0 ||  weights.length != values.length) {
-            return null;
-        }
+        List<Double> weights = availableItems.stream().map(Item::getWeight).toList();
+        List<Double> values = availableItems.stream().map(Item::getValue).toList();
 
         List<Item> itemsInSolution = new ArrayList<>();
         AtomicInteger totalItems = new AtomicInteger();
 
-        int maxProfit = knapsackRecursive(values, weights, maximumWeight, 0, availableItems, totalItems, itemsInSolution);
+        double maxProfit = knapsackRecursive(values, weights, maxWeight, 0, availableItems, totalItems, itemsInSolution);
 
         Knapsack knapsack = new Knapsack(maxWeight);
         knapsack.getItems().addAll(itemsInSolution);
 
-        System.out.printf("# of Solutions: %,d\n, Max Value: $%,.2f", totalItems.longValue(), ((double) maxProfit / 100.0));
+        System.out.printf("# of Solutions: %,d\n, Max Value: $%,.2f", totalItems.longValue(), maxProfit);
 
         return new Knapsack(maxWeight);
 
     }
 
-    private int knapsackRecursive(int[] values, int[] weights, int maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalItems, List<Item> currentItems) {
-        if(maxValue <= 0 || currentIndex >= values.length) {
+    private double knapsackRecursive(List<Double> values, List<Double> weights, double maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalItems, List<Item> currentItems) {
+        if(maxValue <= 0 || currentIndex >= values.size()) {
             // Accumulator - will get total number of solutions. If you want to store all solutions, you'd replace totalItems type with List<List<? extends Item>>.
             totalItems.addAndGet(1);
             return 0;
         }
 
-        int firstValue = 0;
+        double firstValue = 0;
         List<Item> includedItemsWithCurrent = new ArrayList<>(currentItems);
-        if( weights[currentIndex] <= maxValue) {
+        if( weights.get(currentIndex) <= maxValue) {
             includedItemsWithCurrent.add(allItems.get(currentIndex));
-            firstValue = values[currentIndex] + knapsackRecursive(values, weights, maxValue - weights[currentIndex], currentIndex + 1, allItems, totalItems, includedItemsWithCurrent);
+            firstValue = values.get(currentIndex) + knapsackRecursive(values, weights, maxValue - weights.get(currentIndex), currentIndex + 1, allItems, totalItems, includedItemsWithCurrent);
         }
 
         List<Item> includedItemsWithoutCurrent = new ArrayList<>(currentItems);
-        int secondValue = knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalItems, includedItemsWithoutCurrent);
+        double secondValue = knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalItems, includedItemsWithoutCurrent);
 
-        if (firstValue > secondValue) {
+        if(firstValue > secondValue) {
             currentItems.clear();
             currentItems.addAll(includedItemsWithCurrent);
             return firstValue;
