a0a32740f60e044f17460c2d2b39c5bec6a0df2d
Trevor Busk <buskt@ferris.edu>
2025-02-03 20:42:30 -0500

    renaming totalOperations to totalSolutions in OptimalKnapsackAlgorithm for clarity

--- src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
+++ src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
@@ -15,23 +15,23 @@ public class OptimalKnapsackAlgorithm implements KnapsackAlgorithm {
         List<Double> values = availableItems.stream().map(Item::getValue).toList();
 
         List<Item> itemsInSolution = new ArrayList<>();
-        AtomicInteger totalOperations = new AtomicInteger();
+        AtomicInteger totalSolutions = new AtomicInteger();
 
-        double maxProfit = knapsackRecursive(values, weights, maxWeight, 0, availableItems, totalOperations, itemsInSolution);
+        double maxProfit = knapsackRecursive(values, weights, maxWeight, 0, availableItems, totalSolutions, itemsInSolution);
 
         Knapsack knapsack = new Knapsack(maxWeight);
         knapsack.getItems().addAll(itemsInSolution);
 
-        System.out.printf("# of Solutions: %,d\nMax Value: $%,.2f\n", totalOperations.longValue(), maxProfit);
+        System.out.printf("# of Solutions: %,d\nMax Value: $%,.2f\n", totalSolutions.longValue(), maxProfit);
 
         return knapsack;
 
     }
 
-    private double knapsackRecursive(List<Double> values, List<Double> weights, double maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalOperations, List<Item> currentItems) {
+    private double knapsackRecursive(List<Double> values, List<Double> weights, double maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalSolutions, List<Item> currentItems) {
         if(maxValue <= 0 || currentIndex >= values.size()) {
-            // Accumulator - will get total number of solutions. If you want to store all solutions, you'd replace totalOperations type with List<List<? extends Item>>.
-            totalOperations.addAndGet(1);
+            // Accumulator - will get total number of solutions. If you want to store all solutions, you'd replace totalSolutions type with List<List<? extends Item>>.
+            totalSolutions.addAndGet(1);
             return 0;
         }
 
@@ -39,11 +39,11 @@ public class OptimalKnapsackAlgorithm implements KnapsackAlgorithm {
         List<Item> includedItemsWithCurrent = new ArrayList<>(currentItems);
         if( weights.get(currentIndex) <= maxValue) {
             includedItemsWithCurrent.add(allItems.get(currentIndex));
-            firstValue = values.get(currentIndex) + knapsackRecursive(values, weights, maxValue - weights.get(currentIndex), currentIndex + 1, allItems, totalOperations, includedItemsWithCurrent);
+            firstValue = values.get(currentIndex) + knapsackRecursive(values, weights, maxValue - weights.get(currentIndex), currentIndex + 1, allItems, totalSolutions, includedItemsWithCurrent);
         }
 
         List<Item> includedItemsWithoutCurrent = new ArrayList<>(currentItems);
-        double secondValue = knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalOperations, includedItemsWithoutCurrent);
+        double secondValue = knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalSolutions, includedItemsWithoutCurrent);
 
         if(firstValue > secondValue) {
             currentItems.clear();
