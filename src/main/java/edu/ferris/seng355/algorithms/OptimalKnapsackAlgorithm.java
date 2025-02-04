fd00d1146fc3be35ed5290806a1cbc57f6eb41b9
Trevor Busk <buskt@ferris.edu>
2025-02-03 22:33:08 -0500

    refactoring OptimalKnapsackAlgorithm
    - extracted print number of solutions method from collect()
    - removing return value from knapsackRecursive()
    - extracting reset and add all current items logic from knapsackRecursive()

--- src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
+++ src/main/java/edu/ferris/seng355/algorithms/OptimalKnapsackAlgorithm.java
@@ -17,43 +17,51 @@ public class OptimalKnapsackAlgorithm implements KnapsackAlgorithm {
         List<Item> itemsInSolution = new ArrayList<>();
         AtomicInteger totalSolutions = new AtomicInteger();
 
-        double maxProfit = knapsackRecursive(values, weights, maxWeight, 0, availableItems, totalSolutions, itemsInSolution);
+        knapsackRecursive(values, weights, maxWeight, 0, availableItems, totalSolutions, itemsInSolution);
 
         Knapsack knapsack = new Knapsack(maxWeight);
         knapsack.addAllItems(itemsInSolution);
 
-        System.out.printf("# of Solutions: %,d\nMax Value: $%,.2f\n", totalSolutions.longValue(), maxProfit);
+        printNumberOfSolutions(totalSolutions);
 
         return knapsack;
 
     }
 
-    private double knapsackRecursive(List<Double> values, List<Double> weights, double maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalSolutions, List<Item> currentItems) {
+    private static void printNumberOfSolutions(AtomicInteger totalSolutions) {
+        System.out.printf("# of Solutions: %,d\n", totalSolutions.longValue());
+    }
+
+    private void knapsackRecursive(List<Double> values, List<Double> weights, double maxValue, int currentIndex, List<? extends Item> allItems, AtomicInteger totalSolutions, List<Item> currentItems) {
         if(maxValue <= 0 || currentIndex >= values.size()) {
             // Accumulator - will get total number of solutions. If you want to store all solutions, you'd replace totalSolutions type with List<List<? extends Item>>.
             totalSolutions.addAndGet(1);
-            return 0;
+            return;
         }
 
-        double firstValue = 0;
         List<Item> includedItemsWithCurrent = new ArrayList<>(currentItems);
         if( weights.get(currentIndex) <= maxValue) {
             includedItemsWithCurrent.add(allItems.get(currentIndex));
-            firstValue = values.get(currentIndex) + knapsackRecursive(values, weights, maxValue - weights.get(currentIndex), currentIndex + 1, allItems, totalSolutions, includedItemsWithCurrent);
+            knapsackRecursive(values, weights, maxValue - weights.get(currentIndex), currentIndex + 1, allItems, totalSolutions, includedItemsWithCurrent);
         }
 
         List<Item> includedItemsWithoutCurrent = new ArrayList<>(currentItems);
-        double secondValue = knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalSolutions, includedItemsWithoutCurrent);
+        knapsackRecursive(values, weights, maxValue, currentIndex + 1, allItems, totalSolutions, includedItemsWithoutCurrent);
+
+
+        double firstValue = includedItemsWithCurrent.stream().mapToDouble(Item::getValue).sum();
+        double secondValue = includedItemsWithoutCurrent.stream().mapToDouble(Item::getValue).sum();
 
         if(firstValue > secondValue) {
-            currentItems.clear();
-            currentItems.addAll(includedItemsWithCurrent);
-            return firstValue;
+            resetAndAddCurrentItems(currentItems, includedItemsWithCurrent);
         } else {
-            currentItems.clear();
-            currentItems.addAll(includedItemsWithoutCurrent);
-            return secondValue;
+            resetAndAddCurrentItems(currentItems, includedItemsWithoutCurrent);
         }
 
+    }
+
+    private static void resetAndAddCurrentItems(List<Item> currentItems, List<Item> includedItemsWithCurrent) {
+        currentItems.clear();
+        currentItems.addAll(includedItemsWithCurrent);
     }
 }
